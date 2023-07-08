c9520d6f0bffb9135d46a7341b1a459c6108cc8d
Han <tabularasa8931@gmail.com>
2023-07-08 20:39:46 +0900

    Update

--- symlinks.sh
+++ symlinks.sh
@@ -1,9 +1,9 @@
 cp -rs /home/han/.dotfiles/.config/lf /home/han/.config/ 
 cp -rs /home/han/.dotfiles/.config/nvim /home/han/.config/ 
 cp -rs /home/han/.dotfiles/.config/zathura /home/han/.config/
+cp -rs /home/han/.dotfiles/.config/i3 /home/han/.config/
 cp -rs /home/han/.dotfiles/.config/mimeapps.list /home/han/.config/
 
-cp -rs /home/han/.dotfiles/.config/i3 /home/han/.config/
 ln -s /home/han/.dotfiles/.bashrc /home/han/
 ln -s /home/han/.dotfiles/.gitconfig /home/han/
 ln -s /home/han/.dotfiles/.xinitrc /home/han/
