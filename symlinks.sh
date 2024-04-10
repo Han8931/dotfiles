d2bbdeed42ca49d630ff84bf3425b87dcdf5ea7c
Han <tabularasa8931@gmail.com>
2024-04-10 21:54:13 +0900

    Update

--- symlinks.sh
+++ symlinks.sh
@@ -6,10 +6,13 @@ cp -rs /home/han/.dotfiles/.config/mpv /home/han/.config/
 cp -rs /home/han/.dotfiles/.config/mimeapps.list /home/han/.config/
 
 ln -s /home/han/.dotfiles/pacman.conf /etc/
-ln -s /home/han/.dotfiles/golink /usr/bin/
+
 ln -s /home/han/.dotfiles/.gitconfig /home/han/
 ln -s /home/han/.dotfiles/.xinitrc /home/han/
 ln -s /home/han/.dotfiles/.Xmodmap /home/han/
 ln -s /home/han/.dotfiles/.Xresources /home/han/
 ln -s /home/han/.dotfiles/.bashrc /home/han/
 ln -s /home/han/.dotfiles/.bash_profile /home/han/
+
+ln -s /home/han/.dotfiles/golink /usr/bin/
+ln -s /home/han/.dotfiles/empty_trash /home/han/.local/bin/
