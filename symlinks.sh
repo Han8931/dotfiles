3d80e21888cfd9623e8a4bb0f3af7574c91bc26c
Han <tabularasa8931@gmail.com>
2024-06-21 11:10:26 +0900

    Add picom conf

--- symlinks.sh
+++ symlinks.sh
@@ -6,6 +6,7 @@ cp -rs /home/han/.dotfiles/.config/mpv /home/han/.config/
 cp -rs /home/han/.dotfiles/.config/picom /home/han/.config/
 cp -rs /home/han/.dotfiles/.config/mimeapps.list /home/han/.config/
 
+
 ln -s /home/han/.dotfiles/pacman.conf /etc/
 
 ln -s /home/han/.dotfiles/.gitconfig /home/han/
